3ba41ff4628a9e80fcad4115ea0d03c8faa94535
Yassine2052 <mediayh1957@gmail.com>
2025-12-07 18:24:08 +0100

    fix: params pattern regex parsing

--- src/helpers/builders.ts
+++ src/helpers/builders.ts
@@ -3,17 +3,32 @@ import { filenameIsJSorTS, pathExistsAndIsFile } from "./validators";
 import { removeLeadingBackSlashesFromName } from "./cleaners";
 import { REGEX_RIGHT_SLASH_AND_FLAGS_REGEX, REGEX_LEFT_SLASH_REGEX } from "../constants/regex";
 
-export function buildRoutePattern(route: string, name: string, isParam: boolean, pattern?: string | RegExp, isFile: boolean = false) {
+export function buildRoutePattern(
+    route: string,
+    name: string,
+    isParam: boolean,
+    pattern?: string | RegExp,
+    isFile: boolean = false
+) {
     const fileIsIndex = isFile && name.trim().toLowerCase() === "index";
 
     name = fileIsIndex ? "" : buildRouteWithOneLeadingSlash(name.trim());
     route = buildRouteWithOneLeadingSlash(route);
-    pattern = pattern ? `(${pattern.toString().replace(REGEX_LEFT_SLASH_REGEX, "").replace(REGEX_RIGHT_SLASH_AND_FLAGS_REGEX, "")})` : undefined;
 
-    let endpoint =`${route}${name}`;
+    if (pattern) {
+        let raw = pattern.toString()
+            .replace(REGEX_LEFT_SLASH_REGEX, "")
+            .replace(REGEX_RIGHT_SLASH_AND_FLAGS_REGEX, "");
 
-    if(pattern) {
-        endpoint = endpoint + (isParam ? pattern : `/${pattern}`)
+        pattern = `(${raw})`;
+    }
+
+    let endpoint = `${route}${name}`;
+
+    if (pattern) {
+        endpoint = isParam
+            ? `${endpoint}${pattern}`
+            : `${endpoint}/${pattern}`;
     }
 
     return buildRouteWithOneLeadingSlash(endpoint);
